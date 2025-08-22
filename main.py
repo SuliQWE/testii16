506748dbb25aa75463c1bc936956a82209484f70
SuliQWE <bedrocksuli@gmail.com>
2025-08-23 00:35:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-jzuggfubufhbuhz
-zoijbijzfdijhij
-jhkgkgkgkkgkggkgkkgg
-ggkkgkgkgkg
-gkgkk
-gkgkgg
-gkgg
-gg
-gggkkgkgkg
-ggk
-g
-ftftftftftftf
-yuyuyyyyygggggyggyg
-gggygyuhhhuhh
-=======
+
 445454545454
 88787
 788777877
->>>>>>> b0835063d043bb241a57382ab8edb32a58e62c96
+96
